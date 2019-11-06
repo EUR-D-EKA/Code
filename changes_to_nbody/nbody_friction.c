1fa84dbb0bd14c10f35c16d5014a967ad71d7ffa
Autumn <s1erp1nsk1@users.noreply.github.com>
2019-11-06 00:39:47 -0500

    Update DF files

--- changes_to_nbody/nbody_friction.c
+++ changes_to_nbody/nbody_friction.c
@@ -1,7 +1,7 @@
 //File for Dynamical Friction and potentials
 #include "nbody_priv.h"
 #include "nbody_potential.h"
-//#include "nbody_density.h"
+#include "nbody_density.h"
 #include "nbody_potential_types.h"
 #include "milkyway_util.h"
 #include "nbody_caustic.h"
@@ -18,19 +18,16 @@ real isotropic_v_dispersion(real a, real v0, real r){
 
 /** Adds the change in velocity that is the result of DF from a massive body. 
 	Uses the Maxwell's distribution dynamical friction formula.**/
-mwvector dynamicalFriction(mwvector pos, mwvector vel, const Potential* pot){
+mwvector dynamicalFriction(mwvector pos, mwvector vel, real mass, const Potential* pot){
     mwvector result; //Vector with change in velocity due to DF
 
-    real LMC_mass = mw_pow(10, 11) * 1.8*mw_pow(10, 30); //Test mass (for now)
-   	real temp_N = 2000; //Test n bodies (for now) (related to density)
-
     real const G_CONST = 6.67*mw_pow(10, -11); //figure out better place to get this from
-    real k = 1.428; //?
-    real r = mw_pow(mw_dotv(pos,pos), 0.5); //?
-    int a = 12; //?
-    int v0 = 73; //should this be the magnitude of vel
+    real k = 1.428; 
+    real r = mw_pow(mw_dotv(pos,pos), 0.5); 
+    int a = 12; 
+    int v0 = 73; 
 
-    //object velocity , center of gravity is initially at rest
+    //object velocity where center of gravity is initially at rest
     real objectVel = mw_pow(mw_dotv(vel,vel), 0.5); 
     //for Coulomb logarithm
 	real lambda = r / 1.6 / k; 
@@ -39,9 +36,10 @@ mwvector dynamicalFriction(mwvector pos, mwvector vel, const Potential* pot){
     //ratio of the velocity of the object to the modal velocity
     real X = objectVel / (mw_pow(2, 0.5) * sigma);
     //density of the field
-    real density = LMC_mass * temp_N;
+    real density = NFWMHaloDensity(&(pot->halo), r);//milky halo density LMC_mass * temp_N;
     //force from DF
-    real F = (-4*pi*mw_pow(G_CONST, 2)*mw_pow(LMC_mass, 2)* mw_log(lambda)*density / mw_pow(objectVel, 3)) * (erf(X) - 2*X/mw_pow(pi, 0.5)*exp(mw_pow(-1*X, 2)));
+    real F = (-4*pi*mw_pow(G_CONST, 2)*mw_pow(mass, 2)* mw_log(lambda)*density 
+    	/ mw_pow(objectVel, 3)) * (erf(X) - 2*X/mw_pow(pi, 0.5)*exp(mw_pow(-1*X, 2)));
     
     result.x = (F * vel.x / pow(mw_dotv(vel,vel), 0.5));
     result.y = (F * vel.y / pow(mw_dotv(vel,vel), 0.5));
